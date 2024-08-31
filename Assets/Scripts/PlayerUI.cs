81c8c56ecf1a14e95d62c94773138b0f052568e2
Dan Winkler <Dan.Winkler@trojans.dsu.edu>
2024-08-31 13:44:42 -0500

    UI Function Start
    Laid out basic functions for UI, as well as laid out skeleton code in preparation for future features not yet implemented.

---
+++ Assets/Scripts/PlayerUI.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class PlayerUI : MonoBehaviour
+{
+	[SerializeField]
+	private int pLife, pAmmo, pArmor, pItemCount; // these are temporary until the systems for these are actually implemented
+
+	[SerializeField]
+	private TextMeshProUGUI life, ammo, armor, itemCount;
+
+	[SerializeField]
+	private Image ammoIcon, itemIcon, yKey, gKey, bKey; 
+
+	[SerializeField]
+	private GameObject hpIndicator, lerpPointA, lerpPointB;
+
+	private void Update()
+	{
+		life.text = pLife.ToString();
+		ammo.text = pAmmo.ToString();
+		armor.text = pArmor.ToString();
+		itemCount.text = pItemCount.ToString();
+
+		// indicator changes depending on player hp, the more full their hp is the farther right the indicator goes
+		hpIndicator.transform.position = Vector2.Lerp(lerpPointA.transform.position, lerpPointB.transform.position, pLife / 100f);
+
+		// KEY ICONS: Use game action trigger script alongside a special key script to call delegate functions here. Change the image color of a key when its function is called.
+
+
+	}
+
+	// AMMO ICON/COUNT: Have input action functions that pull up the ammo icon on the UI for the respective weapon
+	// Ex: Pressing 2 would pull up the ammo icon for the crystal wand, as well as its current ammo
+	// Make sure each input action function has its own respective icon and ammo count regarding the weapon
+	// If using LB/RB or something similar to cycle weapons on controller, use an index for a switch case or if statement. Decrement/increment index accordingly
+}
