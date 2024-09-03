139792e442a4a3d2d3dcfb99b3177b7679231c3d
SaraSolace88 <sara.a.solace@gmail.com>
2024-09-02 23:35:36 -0500

    Starting Health System Script
    Basic health system script that will most likely require extra components in the future. For now it allows basic tracking of health and damage.

---
+++ Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class HealthSystem : MonoBehaviour
+{
+    #region Fields
+
+    [SerializeField] private bool bPlayer;
+    [SerializeField] private int score;
+    [SerializeField] private int maxHealth = 10;
+
+    [SerializeField]private Image healthBar;
+
+    public int currentHealth;
+    private bool bAlive = true;
+
+    public static Action<int> UpdateScore = delegate { };
+
+    #endregion
+
+
+
+    #region Methods
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void UpdateHealth(int amount)
+    {
+        currentHealth += amount;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+
+        //talk to healthbar
+        if (healthBar)
+        {
+            healthBar.fillAmount = currentHealth / (maxHealth * 1f);
+        }
+
+        if (currentHealth == 0 && bAlive)
+        {
+            bAlive = false;
+            Death();
+        }
+    }
+
+    private void Death()
+    {
+        if (!bPlayer)
+            UpdateScore(score);
+
+    }
+
+    #endregion
+}
