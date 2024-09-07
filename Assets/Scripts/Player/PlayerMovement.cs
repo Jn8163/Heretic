6d109942b199319025a9ab194e91f816795b7d28
SaraSolace88 <sara.a.solace@gmail.com>
2024-09-07 12:20:03 -0500

    Player Movement Changes and Auto Step

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -12,10 +12,30 @@ public class PlayerMovement : MonoBehaviour
 
     [Header("Movement Field(s)")]
     [SerializeField] private float speed = 6f;
+    [SerializeField] private bool animateCam = true;
+    [SerializeField]
+
     private InputSystem pInput;
     private Rigidbody rb;
-    private Vector3 direction;
     private Animator cameraAnim;
+
+    private GameObject groundCheck;
+    private bool grounded = true;
+
+    private Vector3 direction;
+
+
+
+    [Header("Auto-Step Field(s)")]
+
+    [Tooltip("The max vertical length of step the player will automatically take.")]
+    [SerializeField] private float stepHeight = .3f;
+
+    [Tooltip("How smooth auto-step's will be: Smaller = smoother")]
+    [SerializeField] private float stepRate = .1f;
+
+    private GameObject stepRayLower, stepRayUpper;
+
 
     #endregion
 
@@ -23,20 +43,25 @@ public class PlayerMovement : MonoBehaviour
 
     #region Methods
 
+    private void Awake()
+    {
+        cameraAnim = transform.Find("CameraParent").transform.Find("Camera").GetComponent<Animator>();
+        stepRayLower = transform.Find("StepRayLower").gameObject;
+        stepRayUpper = transform.Find("StepRayUpper").gameObject;
+        groundCheck = transform.Find("GroundCheck").gameObject;
+        rb = GetComponent<Rigidbody>();
+
+        stepRayUpper.transform.localPosition = new Vector3(0, stepHeight, 0);
+    }
+
+
+
     private void OnEnable()
     {
         pInput = new InputSystem();
         pInput.Enable();
 
-        rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
-    }
-
-
-
-    private void Start()
-    {
-        cameraAnim = transform.Find("CameraParent").transform.Find("Camera").GetComponent<Animator>();
     }
 
 
@@ -50,37 +75,83 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        //GroundCheck();
         Move();
     }
 
-
+    
+    //In progress
+    private void GroundCheck()
+    {
+        Debug.DrawLine(groundCheck.transform.position, groundCheck.transform.position - new Vector3(0, .3f, 0), Color.red, .1f);
+        if(Physics.BoxCast(groundCheck.transform.position, new Vector3(.5f,.01f,.5f), Vector3.down, out RaycastHit hit, Quaternion.identity, .5f))
+        {
+            if(!(hit.point.y + .1f < transform.position.y))
+            {
+                grounded = true;
+            }
+        }
+        else
+        {
+            grounded = false;
+        }
+    }
 
     private void Move()
     {
         direction.x = pInput.Player.Move.ReadValue<Vector2>().x;
-        direction.y = 0;
         direction.z = pInput.Player.Move.ReadValue<Vector2>().y;
 
-        //Normalizes movement for any direction - needed for camera rotation
         direction = transform.right * direction.x + transform.forward * direction.z;
-        if(direction != Vector3.zero)
+        direction.Normalize();
+
+        if (direction != Vector3.zero)
         {
-            cameraAnim.SetBool("moving", true);
+            rb.AddForce( direction * speed, ForceMode.VelocityChange);
+
+            if (animateCam)
+            {
+                cameraAnim.SetBool("moving", true);
+            }
+
+            AutoStep();
         }
         else
         {
             cameraAnim.SetBool("moving", false);
         }
+    }
 
-        rb.AddForce(direction.normalized * speed, ForceMode.VelocityChange);
+
+
+    private void AutoStep()
+    {
+        if (Physics.BoxCast(stepRayLower.transform.position, new Vector3(.1f,.9f, .1f), direction, Quaternion.identity, 1f))
+        {
+            if (!Physics.BoxCast(stepRayUpper.transform.position, new Vector3(.1f, .1f, .1f), direction, Quaternion.identity, 1f))
+            {
+                float boxCastHeightInc = .1f;
+                float objectHeight = 0;
+                while(Physics.BoxCast(stepRayLower.transform.position + new Vector3(0, boxCastHeightInc, 0), new Vector3(.1f, .1f, .1f), direction, Quaternion.identity, 1f))
+                {
+                    objectHeight = boxCastHeightInc;
+                    boxCastHeightInc += .1f;
+                }
+                rb.position += new Vector3(0f, objectHeight, 0f);
+            }
+        }
     }
 
     #endregion
-
 }
 
 /* Refrences:
+ * Basic Movement:
  * https://www.youtube.com/watch?v=LqnPeqoJRFY
  * Utilized physic based movement and considered other properties in video.
  * Decided agaisnt implementing drag since Rigidbody component already has options for drag.
+ * 
+ * Auto Step Up:
+ * https://www.youtube.com/watch?v=DrFk5Q_IwG0&t=547s
+ * Major rework of code was done, but basics we're found here.
  */
