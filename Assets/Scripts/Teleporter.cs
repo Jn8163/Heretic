e3ea1dd708121ea85645d6d149105e2ac3fd2063
WJK3080 <wyatt.krebs@trojans.dsu.edu>
2024-09-26 14:54:35 -0500

    Teleport Script

--- Assets/Scripts/Teleporter.cs
+++ Assets/Scripts/Teleporter.cs
@@ -2,6 +2,19 @@ using UnityEngine;
 
 public class Teleporter : MonoBehaviour
 {
+    [SerializeField]
+    public Transform playerTPLoc;
+    [SerializeField]
+    public Transform destination;
+    [SerializeField]
+    public GameObject playerTP;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        playerTP.SetActive(false);
+        playerTPLoc.position = destination.position;
+        playerTP.SetActive(true);
+    }
     void Start()
     {
         
