e0e5a8fdd6c31d332dcee2efdcba5a85f48f20a0
Dan Winkler <Dan.Winkler@trojans.dsu.edu>
2024-10-08 14:46:32 -0500

    Door Meshes different color, switch door in docks lowers, increment scene script

---
+++ Assets/Scripts/Systems/IncrementScene.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class IncrementScene : MonoBehaviour
+{
+    private PlayerInput pInput;
+    private bool isInteractable;
+
+	[SerializeField]
+	private string sceneName;
+
+	private void Start()
+	{
+		pInput = new PlayerInput();
+		pInput.Enable();
+
+		pInput.Player.Interact.performed += SwitchScene;
+	}
+
+	private void OnDisable()
+	{
+		pInput.Player.Interact.performed -= SwitchScene;
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			isInteractable = true;
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			isInteractable = false;
+		}
+	}
+
+	private void SwitchScene(InputAction.CallbackContext c)
+	{
+		if (isInteractable)
+		{
+			SceneManager.LoadScene(sceneName);
+		}
+	}
+}
