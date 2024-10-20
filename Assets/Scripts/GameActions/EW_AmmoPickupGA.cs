6019e977207aea69a7d4109300a69fba6f06e31d
Jasper Waddell <kaden.waddell@trojans.dsu.edu>
2024-10-20 15:17:47 -0500

    ItemPickup Sound Stuff

--- Assets/Scripts/GameActions/EW_AmmoPickupGA.cs
+++ Assets/Scripts/GameActions/EW_AmmoPickupGA.cs
@@ -8,12 +8,15 @@ public class EW_AmmoPickupGA : GameAction
 	private ElvenWand elvenWand;
 
 	[SerializeField] private GameObject mesh, sprite;
+	[SerializeField] private AudioSource audioSource;
 
 	public int ammo_amount = 1;
 
     public override void Action()
     {
 		elvenWand.current_ammo += ammo_amount;
+		audioSource.pitch = UnityEngine.Random.Range(0.9f, 1.1f);
+		audioSource.Play();
 		StartCoroutine(DestroyPickup());
     }
 
