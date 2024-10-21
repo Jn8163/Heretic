a2caa82c92ff9c359998ce77f8aa34b92b5a388b
Jasper Waddell <kaden.waddell@trojans.dsu.edu>
2024-10-21 03:30:07 -0500

    Item Pickup Soundbytes!

--- Assets/Scripts/GameActions/EC_AmmoPickupGA.cs
+++ Assets/Scripts/GameActions/EC_AmmoPickupGA.cs
@@ -6,12 +6,15 @@ using UnityEngine.ProBuilder.Shapes;
 public class EC_AmmoPickupGA : GameAction
 {
 	[SerializeField] private GameObject mesh, sprite;
+    [SerializeField] private AudioSource audioSource;
 
-	public int ammo_amount = 10;
+    public int ammo_amount = 10;
 
     public override void Action()
     {
-		StartCoroutine(DestroyPickup());
+        audioSource.pitch = UnityEngine.Random.Range(0.9f, 1.1f);
+        audioSource.Play();
+        StartCoroutine(DestroyPickup());
     }
 
     IEnumerator DestroyPickup()
--- Assets/Scripts/Menu&UI/PickupTrigger.cs
+++ Assets/Scripts/Menu&UI/PickupTrigger.cs
@@ -5,9 +5,13 @@ public class PickupTrigger : MonoBehaviour
 {
     public static Action<Item, GameObject> PickUpObject = delegate { };
     [SerializeField] private Item pickup;
+    [SerializeField] private AudioSource audioSource;
 
     private void OnTriggerEnter(Collider other)
     {
+        audioSource.pitch = UnityEngine.Random.Range(0.9f, 1.1f);
+        audioSource.Play();
+
         PickUpObject(pickup, gameObject);
     }
 }
