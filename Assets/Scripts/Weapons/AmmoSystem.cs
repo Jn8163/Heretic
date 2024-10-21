ced91ab49d251132ba2630af29a6067e38dbff8e
WJK3080 <wyatt.krebs@trojans.dsu.edu>
2024-10-21 08:52:37 -0500

    Adding spawn system as prefab

---
+++ Assets/Scripts/Weapons/AmmoSystem.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class AmmoSystem : MonoBehaviour
+{
+    int ElvenWandAmmo;
+    int ElvenWandAmmoMax = 30;
+    int EtherealCrossbowAmmo;
+    int EtherealCrossbowAmmoMax;
+
+    void Start()
+    {
+        ElvenWandAmmo = 10;
+    }
+
+    void Update()
+    {
+        
+    }
+}
