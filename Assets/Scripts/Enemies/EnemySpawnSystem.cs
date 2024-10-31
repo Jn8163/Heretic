762352556099ed010a01b476ddef79d06afd4540
WJK3080 <wyatt.krebs@trojans.dsu.edu>
2024-10-31 13:48:49 -0500

    Added Enemies to Level

--- Assets/Scripts/Enemies/EnemySpawnSystem.cs
+++ Assets/Scripts/Enemies/EnemySpawnSystem.cs
@@ -7,15 +7,35 @@ public class EnemySpawnSystem : MonoBehaviour
     [SerializeField]
     public List<GameObject> GargoyleSpawns = new List<GameObject>();
     [SerializeField] GameObject GargoylePrefab;
-    int GarSpawnNum;
+    int SpawnNum;
+    [SerializeField]
+    public List<GameObject> GolemSpawns = new List<GameObject>();
+    //golem prefab
+    [SerializeField]
+    public List<GameObject> SkeletonSpawns = new List<GameObject>();
+    //skeleton warrior prefab
+
     void Start()
     {
-        GarSpawnNum = -1;
+        SpawnNum = -1;
         foreach(GameObject g in GargoyleSpawns)
         {
-            Instantiate(GargoylePrefab, GargoyleSpawns[GarSpawnNum + 1].transform.position, GargoyleSpawns[GarSpawnNum + 1].transform.rotation);
-            GarSpawnNum++;
+            Instantiate(GargoylePrefab, GargoyleSpawns[SpawnNum + 1].transform.position, GargoyleSpawns[SpawnNum + 1].transform.rotation);
+            SpawnNum++;
         }
+        SpawnNum = -1;
+        foreach( GameObject g in GolemSpawns)
+        {
+            Instantiate(GargoylePrefab, GolemSpawns[SpawnNum + 1].transform.position, GolemSpawns[SpawnNum + 1].transform.rotation);
+            SpawnNum++;
+        }
+        SpawnNum = -1;
+        foreach (GameObject g in SkeletonSpawns)
+        {
+            Instantiate(GargoylePrefab, SkeletonSpawns[SpawnNum + 1].transform.position, SkeletonSpawns[SpawnNum + 1].transform.rotation);
+            SpawnNum++;
+        }
+        SpawnNum = -1;
     }
 
 }
