c6f1c0c51632326d81cc46c4c717835c433cd641
Jesse Ray <91559806+jesseray1450@users.noreply.github.com>
2025-02-03 11:26:55 -0600

    made some changes to the golem and gargoyle movement

--- Assets/Scripts/Enemies/EnemyGargoyleAI.cs
+++ Assets/Scripts/Enemies/EnemyGargoyleAI.cs
@@ -1,22 +1,30 @@
 using UnityEngine;
 using System.Collections;
-using Unity.VisualScripting;
 
 public class EnemyGargoyleAI : EnemyBaseClass
 {
     [SerializeField] private float hoverDist;
     [SerializeField] private float DashRange = 11;
+    [SerializeField] private float randomMoveInterval = 1.2f;
+    [SerializeField] private float hoverMoveRadius = 2f;
+    [SerializeField] private float dodgeChance = 0.3f; // 30% chance to dodge
+
     private RaycastHit hit;
     private Vector3 pos;
 
     protected override void Update()
     {
-        // Keep the Gargoyle hovering at a set height above the ground
+        // Keep hovering at a set height above the ground
         if (Physics.Raycast(transform.position, -transform.up, out hit))
         {
             pos = transform.position;
             pos.y = hit.point.y + hoverDist;
             transform.position = pos;
+        }
+
+        if (isChasing)
+        {
+            StartCoroutine(RandomHoverMovement());
         }
 
         base.Update();
@@ -37,6 +45,32 @@ public class EnemyGargoyleAI : EnemyBaseClass
             }
         }
         base.ChasePlayer();
+    }
+
+    IEnumerator RandomHoverMovement()
+    {
+        while (isChasing)
+        {
+            // Slightly random movement in air
+            Vector3 randomOffset = new Vector3(
+                Random.Range(-hoverMoveRadius, hoverMoveRadius),
+                Random.Range(-hoverMoveRadius, hoverMoveRadius),
+                Random.Range(-hoverMoveRadius, hoverMoveRadius)
+            );
+
+            Vector3 newTarget = player.position + randomOffset;
+            agent.SetDestination(newTarget);
+
+            // Occasional dodge to left/right
+            if (Random.value < dodgeChance)
+            {
+                Vector3 dodgeDirection = (Random.value < 0.5f) ? transform.right : -transform.right;
+                Vector3 jukeTarget = transform.position + dodgeDirection * hoverMoveRadius;
+                agent.SetDestination(jukeTarget);
+            }
+
+            yield return new WaitForSeconds(randomMoveInterval);
+        }
     }
 
     IEnumerator resetSpeed()
--- Assets/Scripts/Enemies/Golem.cs
+++ Assets/Scripts/Enemies/Golem.cs
@@ -1,12 +1,37 @@
+using System.Collections;
+using UnityEngine;
+
 public class Golem : EnemyBaseClass
 {
-    protected override void Awake()
-    {
-        base.Awake();
-    }
+    [SerializeField] private float zigZagInterval = 1.5f; // Time between zig-zag switches
+    [SerializeField] private float zigZagAmount = 3f; // How far to move sideways
+
+    private bool movingRight = true;
 
     protected override void Update()
     {
         base.Update();
+
+        if (isChasing)
+        {
+            StartCoroutine(ZigZagMovement());
+        }
+    }
+
+    IEnumerator ZigZagMovement()
+    {
+        while (isChasing)
+        {
+            // Compute sideways movement
+            Vector3 lateralMovement = transform.right * (movingRight ? 1 : -1) * zigZagAmount;
+            Vector3 newTarget = player.position + lateralMovement;
+
+            // Move toward the zig-zag point
+            agent.SetDestination(newTarget);
+
+            // Switch direction
+            movingRight = !movingRight;
+            yield return new WaitForSeconds(zigZagInterval);
+        }
     }
 }
