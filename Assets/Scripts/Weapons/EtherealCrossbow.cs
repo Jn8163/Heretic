c4022c6511039b735a1c87c087cbf425436d3d83
Tostahh <114684478+Tostahh@users.noreply.github.com>
2025-02-02 18:58:06 -0600

    CrossBow Done
    Crossbow weapon is in, it just needs its pick up item set up

--- Assets/Scripts/Weapons/EtherealCrossbow.cs
+++ Assets/Scripts/Weapons/EtherealCrossbow.cs
@@ -4,8 +4,14 @@ using UnityEngine.InputSystem;
 
 public class EtherealCrossbow : RangedWeapon
 {
+    [SerializeField] private GameObject SmallboltPrefab;
+
+    [SerializeField] private GameObject SmallBoltFiringpoint1;
+    [SerializeField] private GameObject SmallBoltFiringpoint2;
+
     [SerializeField] private float BigBoltSpeed;
     [SerializeField] private float SmallBoltSpeed;
+
 
     protected override void OnEnable()
     {
@@ -33,10 +39,16 @@ public class EtherealCrossbow : RangedWeapon
         base.Attack(c);
 
         if (!cooldown && ammoSystem.UpdateAmmo(ammoType, ammoUsage, false))
-        {
+        { 
             StartCoroutine(nameof(WeaponCooldown));
             Projectile P = Instantiate(projectilePFab, transform.position, transform.rotation).GetComponent<Projectile>();
             P.SetProjectile(BigBoltSpeed, transform.forward);
+
+            Projectile sP1 = Instantiate(SmallboltPrefab, SmallBoltFiringpoint1.transform.position, SmallBoltFiringpoint1.transform.rotation).GetComponent<Projectile>();
+            sP1.SetProjectile(SmallBoltSpeed, SmallBoltFiringpoint1.transform.forward);
+
+            Projectile sP2 = Instantiate(SmallboltPrefab, SmallBoltFiringpoint2.transform.position, SmallBoltFiringpoint2.transform.rotation).GetComponent<Projectile>();
+            sP2.SetProjectile(SmallBoltSpeed, SmallBoltFiringpoint2.transform.forward);
         }
     }
     protected override IEnumerator WeaponCooldown()
