3f131926ed27fafeba6b83fc807109674f6b797d
Dan Winkler <Dan.Winkler@trojans.dsu.edu>
2025-02-11 22:38:20 -0600

    Crossbow Tome of Power

--- Assets/Scripts/Weapons/EtherealCrossbow.cs
+++ Assets/Scripts/Weapons/EtherealCrossbow.cs
@@ -8,6 +8,8 @@ public class EtherealCrossbow : RangedWeapon
 
     [SerializeField] private GameObject SmallBoltFiringpoint1;
     [SerializeField] private GameObject SmallBoltFiringpoint2;
+    [SerializeField] private GameObject SmallBoltFiringpoint3;
+    [SerializeField] private GameObject SmallBoltFiringpoint4;
 
     [SerializeField] private float BigBoltSpeed;
     [SerializeField] private float SmallBoltSpeed;
@@ -38,18 +40,42 @@ public class EtherealCrossbow : RangedWeapon
     {
         base.Attack(c);
 
-        if (!cooldown && ammoSystem.UpdateAmmo(ammoType, ammoUsage, false))
-        { 
-            StartCoroutine(nameof(WeaponCooldown));
-            Projectile P = Instantiate(projectilePFab, transform.position, transform.rotation).GetComponent<Projectile>();
-            P.SetProjectile(BigBoltSpeed, transform.forward);
+        if (!ActivateTome.isCharged)
+        {
+            if (!cooldown && ammoSystem.UpdateAmmo(ammoType, ammoUsage, false))
+            {
+                StartCoroutine(nameof(WeaponCooldown));
+                Projectile P = Instantiate(projectilePFab, transform.position, transform.rotation).GetComponent<Projectile>();
+                P.SetProjectile(BigBoltSpeed, transform.forward);
 
-            Projectile sP1 = Instantiate(SmallboltPrefab, SmallBoltFiringpoint1.transform.position, SmallBoltFiringpoint1.transform.rotation).GetComponent<Projectile>();
-            sP1.SetProjectile(SmallBoltSpeed, SmallBoltFiringpoint1.transform.forward);
+                Projectile sP1 = Instantiate(SmallboltPrefab, SmallBoltFiringpoint1.transform.position, SmallBoltFiringpoint1.transform.rotation).GetComponent<Projectile>();
+                sP1.SetProjectile(SmallBoltSpeed, SmallBoltFiringpoint1.transform.forward);
 
-            Projectile sP2 = Instantiate(SmallboltPrefab, SmallBoltFiringpoint2.transform.position, SmallBoltFiringpoint2.transform.rotation).GetComponent<Projectile>();
-            sP2.SetProjectile(SmallBoltSpeed, SmallBoltFiringpoint2.transform.forward);
+                Projectile sP2 = Instantiate(SmallboltPrefab, SmallBoltFiringpoint2.transform.position, SmallBoltFiringpoint2.transform.rotation).GetComponent<Projectile>();
+                sP2.SetProjectile(SmallBoltSpeed, SmallBoltFiringpoint2.transform.forward);
+            }
         }
+        else
+        {
+			if (!cooldown && ammoSystem.UpdateAmmo(ammoType, ammoUsage, false))
+			{
+				StartCoroutine(nameof(WeaponCooldown));
+				Projectile P1 = Instantiate(projectilePFab, transform.position, transform.rotation).GetComponent<Projectile>();
+				P1.SetProjectile(BigBoltSpeed, transform.forward);
+
+				Projectile P2 = Instantiate(projectilePFab, SmallBoltFiringpoint1.transform.position, SmallBoltFiringpoint1.transform.rotation).GetComponent<Projectile>();
+				P2.SetProjectile(SmallBoltSpeed, SmallBoltFiringpoint1.transform.forward);
+
+				Projectile P3 = Instantiate(projectilePFab, SmallBoltFiringpoint2.transform.position, SmallBoltFiringpoint2.transform.rotation).GetComponent<Projectile>();
+				P3.SetProjectile(SmallBoltSpeed, SmallBoltFiringpoint2.transform.forward);
+
+				Projectile sP1 = Instantiate(SmallboltPrefab, SmallBoltFiringpoint3.transform.position, SmallBoltFiringpoint3.transform.rotation).GetComponent<Projectile>();
+				sP1.SetProjectile(SmallBoltSpeed, SmallBoltFiringpoint3.transform.forward);
+
+				Projectile sP2 = Instantiate(SmallboltPrefab, SmallBoltFiringpoint4.transform.position, SmallBoltFiringpoint4.transform.rotation).GetComponent<Projectile>();
+				sP2.SetProjectile(SmallBoltSpeed, SmallBoltFiringpoint4.transform.forward);
+			}
+		}
     }
     protected override IEnumerator WeaponCooldown()
     {
